0e17d6edd495f03881e3e619ae80cb1563bd76b1
msjin123 <158466791+msjin123@users.noreply.github.com>
2026-02-23 22:29:45 +0900

    map 활용
    이진 트리 기반의 삽입/삭제 O(log n) 인 map에 대해 새로 알았다.
    아무 값이나 리스트랑 비슷하게 키,값 형태로 저장할 수 있다.
    이 문제에서 아무 값의 실제 개수를 계속 바꿀때 map을 써야 풀 수 있었다..
    unordered_map은 map과는 다르게 정렬이 되어 있지않은 맵으로 파이썬의 딕셔너리와 완전 같다고 봐도 무방하다 (~~[key]=value) 형태
    
    이 문제를 풀 떄, 이상한 요행을 부리면 무조건 예외가 나오고, 계쏙 두 우선순위 큐를 동기화 하는게 중요했다. 완전히 동기화 하는것이 아니라, 위에 큐에는 없는 원소가 아래 큐에 있으면 혹은 그 반대면 잘못된 최대/최솟값을 출력하기 때문에, 그 수(key)의 개수가 0(value) 면 실시간으로 다른 큐에있는 그 수를 전부 제거하는 작업을 해주면 된다..
    실제 개수는 1개인데 큐에는 2개 있는 그런거는 최대/최솟값만 구하는 이 문제에 영향을 끼치지 않는다...
    인터넷을 보면서 이렇게 이해했다.

---
+++ gold/7662_이중 우선순위 큐.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int t;
+	cin >> t;
+	for (int i = 0; i < t; i++) {
+		int k;
+		cin >> k;
+		priority_queue<int> pq;							  // 원래 queue
+		priority_queue<int,vector<int>,greater<int>> pq2; // 내림차순 저장 queue
+		int ins = 0;
+		int del = 0;
+		map<int, int> cnt;
+		cnt.clear();
+		for (int j = 0; j < k; j++) {
+			char choice;
+			int num;
+			cin >> choice;
+			cin >> num;
+			
+			if (choice == 'I') {
+				pq.push(num);
+				pq2.push(num);
+				ins++;
+				cnt[num] += 1;
+			}
+			// 삭제 횟수 >= 삽입 횟수이면 queue 가 비어있다는 뜻이기 때문에 시행 x
+			else if (choice == 'D') {
+				if (del < ins) {
+					if (num == 1) { // 최댓값 삭제
+						cnt[pq.top()] -= 1;
+						pq.pop();
+						del++;
+					}
+					else if (num == -1) { // 최솟값 삭제
+
+						cnt[pq2.top()] -= 1;
+						pq2.pop();
+						del++;
+						
+					}
+				}
+				while (!pq.empty() && cnt[pq.top()] == 0) pq.pop();
+				while (!pq2.empty() && cnt[pq2.top()] == 0) pq2.pop();
+			}
+
+		}
+		while (!pq.empty() && cnt[pq.top()] == 0) pq.pop();
+		while (!pq2.empty() && cnt[pq2.top()] == 0) pq2.pop();
+		
+		if (del >= ins) {
+			cout << "EMPTY";
+		}
+		else if (del < ins) {   // pq,pq2, 각각에 원소가 최소 하나 이상 있음
+			cout << pq.top() << " " << pq2.top();
+		}
+		cout << '\n';
+	}
+	return 0;
+}
