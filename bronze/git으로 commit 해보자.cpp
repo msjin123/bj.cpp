85da0c1651158c8c6bf7a5cc687b705610eccb7b
msjin123 <vvvvv72@pusan.ac.kr>
2026-04-15 10:04:54 +0900

    git을 이용해서 올려보기

---
+++ bronze/git으로 commit 해보자.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <string>
+using namespace std;
+int main(){
+   
+    int h,m;
+    cin >> h >> m;
+
+    if (m>=45){
+        cout << h << " " << m-45;
+    }
+    else{
+        if(h==0){
+            cout << 23 << " " << m+15;
+        }
+        else{
+            cout << h-1 << " " << m+15;
+        }
+    }
+
+    
+
+    return 0;
+}
